b19ce8474defbe142037e017c87b649dc3a031fd
Enrique Bonilla <ebonillau@proveedor.intercorp.com.pe>
2025-11-08 02:53:03 -0500

    Initial Commit

---
+++ InterbankClone/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  InterbankClone
+//
+//  Created by Enrique Bonilla on 8/11/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ InterbankCloneTests/InterbankCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  InterbankCloneTests.swift
+//  InterbankCloneTests
+//
+//  Created by Enrique Bonilla on 8/11/25.
+//
+
+import Testing
+@testable import InterbankClone
+
+struct InterbankCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
